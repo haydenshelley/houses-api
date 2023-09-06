885786facd5d180f21568cd59b8eb5e43a888214
Hayden Shelley <hayden.m.shelley@gmail.com>
2023-09-06 12:34:48 -0700

    build houses create

--- app/controllers/houses_controller.rb
+++ app/controllers/houses_controller.rb
@@ -3,4 +3,14 @@ class HousesController < ApplicationController
     @houses = House.all 
     render :index
   end
+
+  def create
+    @house = House.create(
+      sqft: params[:sqft],
+      bedrooms: params[:bedrooms],
+      bathrooms: params[:bathrooms],
+      address: params[:address]
+    )
+    render :show
+  end
 end
